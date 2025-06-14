14322551825fda4bbbb3b242c40c450a351c75c5
Jonny Hicks <hello.jonnyhicks@gmail.com>
2025-06-14 10:46:55 +0200

    Fix typo in user property assignment in post route

--- routes/thoughts.js
+++ routes/thoughts.js
@@ -79,7 +79,7 @@ router.post("/", authenticationUser, async (req, res) => {
     const newThought = await Thought.create({
       message,
       hearts: hearts || 0,
-      user: req.user.usernamn,
+      user: req.user.username,
     });
     res.status(201).json(newThought);
   } catch (err) {
